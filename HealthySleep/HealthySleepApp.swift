62d9085280c619785a7e581ce72e0c6d495faaad
Cengizhan55 <cengiz_er_@hotmail.com>
2022-03-28 12:06:30 +0300

    Initial commit

---
+++ HealthySleep/HealthySleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthySleepApp.swift
+//  HealthySleep
+//
+//  Created by Cengizhan Er on 27.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct HealthySleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
